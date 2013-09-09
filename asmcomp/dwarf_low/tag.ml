fa83cddc01bfcdd9467fec4ef15a0323e20c7eb3
Thomas Refis <thomas.refis@gmail.com>
2013-09-09 14:51:54 +0100

    adds lexical block dwarf tag

--- asmcomp/dwarf_low/tag.ml
+++ asmcomp/dwarf_low/tag.ml
@@ -27,6 +27,7 @@ type t =
   | DW_TAG_formal_parameter
   | DW_TAG_variable
   | DW_TAG_base_type
+  | DW_TAG_lexical_block
 
 let encode t =
   let code =
@@ -37,6 +38,7 @@ let encode t =
     | DW_TAG_formal_parameter -> 0x05
     | DW_TAG_variable -> 0x34
     | DW_TAG_base_type -> 0x24
+    | DW_TAG_lexical_block -> 0x0b
   in
   Value.as_uleb128 code
 
@@ -49,6 +51,7 @@ let child_determination = function
   | DW_TAG_formal_parameter -> Child_determination.no
   | DW_TAG_variable -> Child_determination.no
   | DW_TAG_base_type -> Child_determination.no
+  | DW_TAG_lexical_block -> Child_determination.no
 
 let compile_unit = DW_TAG_compile_unit
 let subprogram = DW_TAG_subprogram
@@ -56,6 +59,7 @@ let subprogram_with_no_children = DW_TAG_subprogram__no_children
 let formal_parameter = DW_TAG_formal_parameter
 let variable = DW_TAG_variable
 let base_type = DW_TAG_base_type
+let lexical_block = DW_TAG_lexical_block
 
 let size t =
   Value.size (encode t)
--- asmcomp/dwarf_low/tag.mli
+++ asmcomp/dwarf_low/tag.mli
@@ -30,5 +30,6 @@ val subprogram_with_no_children : t
 val formal_parameter : t
 val variable : t
 val base_type : t
+val lexical_block : t
 
 val child_determination : t -> Child_determination.t
