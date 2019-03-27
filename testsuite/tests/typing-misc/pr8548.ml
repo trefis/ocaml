06bf9961dceb279cdb670882e47bd854ef0a965c
Thomas Refis <thomas.refis@gmail.com>
2019-03-27 15:49:05 +0800

    explicitely scope when freshening sig in includemod

--- testsuite/tests/typing-misc/pr8548.ml
+++ testsuite/tests/typing-misc/pr8548.ml
@@ -39,98 +39,18 @@ module type Ranged = sig
 end
 ;;
 [%%expect{|
-Line 1:
-Error: Module type declarations do not match:
-         module type Ranged =
-           sig
-             module Endpoint : Endpoint_intf
-             module Range :
-               sig
-                 module Endpoint : sig type t = Endpoint.t end
-                 type finite = [ `Before of Endpoint.t ]
-                 type infinite = [ `Until_infinity ]
-                 type +'a range = private { until : 'a; }
-                   constraint 'a =
-                     [< `Before of Endpoint.t | `Until_infinity ]
-                 val until :
-                   ([< `Before of Endpoint.t | `Until_infinity ] as 'a) range ->
-                   'a
-               end
-           end
-       does not match
-         module type Ranged =
-           sig
-             module Endpoint : Endpoint_intf
-             module Range :
-               sig
-                 module Endpoint : sig type t = Endpoint.t end
-                 type finite = [ `Before of Endpoint.t ]
-                 type infinite = [ `Until_infinity ]
-                 type +'a range = private { until : 'a; }
-                   constraint 'a =
-                     [< `Before of Endpoint.t | `Until_infinity ]
-                 val until :
-                   ([< `Before of Endpoint.t | `Until_infinity ] as 'a) range ->
-                   'a
-               end
-           end
-       At position module type Ranged = <here>
-       Modules do not match:
-         sig
-           module Endpoint : Endpoint_intf
-           module Range :
-             sig
-               module Endpoint : sig type t = Endpoint.t end
-               type finite = [ `Before of Endpoint.t ]
-               type infinite = [ `Until_infinity ]
-               type +'a range = private { until : 'a; }
-                 constraint 'a = [< `Before of Endpoint.t | `Until_infinity ]
-               val until :
-                 ([< `Before of Endpoint.t | `Until_infinity ] as 'a) range ->
-                 'a
-             end
-         end
-       is not included in
-         sig
-           module Endpoint : Endpoint_intf
-           module Range :
-             sig
-               module Endpoint : sig type t = Endpoint.t end
-               type finite = [ `Before of Endpoint.t ]
-               type infinite = [ `Until_infinity ]
-               type +'a range = private { until : 'a; }
-                 constraint 'a = [< `Before of Endpoint.t | `Until_infinity ]
-               val until :
-                 ([< `Before of Endpoint.t | `Until_infinity ] as 'a) range ->
-                 'a
-             end
-         end
-       At position module type Ranged = sig module Range : <here> end
-       Modules do not match:
-         sig
-           module Endpoint = Range.Endpoint
-           type finite = [ `Before of Endpoint.t ]
-           type infinite = [ `Until_infinity ]
-           type +'a range = 'a Range.range = private { until : 'a; }
-             constraint 'a = [< `Before of Endpoint.t | `Until_infinity ]
-           val until :
-             ([< `Before of Endpoint.t | `Until_infinity ] as 'a) range -> 'a
-         end
-       is not included in
-         sig
-           module Endpoint : sig type t = Endpoint.t end
-           type finite = [ `Before of Endpoint.t ]
-           type infinite = [ `Until_infinity ]
-           type +'a range = private { until : 'a; }
-             constraint 'a = [< `Before of Endpoint.t | `Until_infinity ]
-           val until :
-             ([< `Before of Endpoint.t | `Until_infinity ] as 'a) range -> 'a
-         end
-       At position module type Ranged = sig module Range : <here> end
-       Values do not match:
-         val until :
-           ([< `Before of Endpoint.t | `Until_infinity ] as 'a) range -> 'a
-       is not included in
-         val until :
-           ([< `Before of Endpoint.t | `Until_infinity ] as 'a) range -> 'a
+module type Ranged =
+  sig
+    module Endpoint : Endpoint_intf
+    module Range :
+      sig
+        module Endpoint : sig type t = Endpoint.t end
+        type finite = [ `Before of Endpoint.t ]
+        type infinite = [ `Until_infinity ]
+        type +'a range = private { until : 'a; }
+          constraint 'a = [< `Before of Endpoint.t | `Until_infinity ]
+        val until :
+          ([< `Before of Endpoint.t | `Until_infinity ] as 'a) range -> 'a
+      end
+  end
 |}]
