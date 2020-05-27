aab2a6158f0e1f4c964664f9d9d205eb9212b997
Thomas Refis <thomas.refis@gmail.com>
2020-05-27 19:40:09 +0200

    tests/modular_implicits: enable some broken tests

--- testsuite/tests/typing-modular_implicits/arithmetic.ml
+++ testsuite/tests/typing-modular_implicits/arithmetic.ml
@@ -1,29 +1,52 @@
+(* TEST
+   * expect
+*)
+
+type z = Z
+type 'n s = S of 'n;;
+[%%expect{|
 type z = Z
 type 'n s = S of 'n
+|}];;
 
 module type N = sig
   type n
   val n : n
-end
+end;;
+[%%expect{|
+module type N = sig type n val n : n end
+|}];;
 
 implicit module Z : N with type n = z = struct
   type n = z
   let n = Z
-end
+end;;
+[%%expect{|
+module Z : sig type n = z val n : n end
+|}];;
 
 implicit module S {N : N} : N with type n = N.n s = struct
   type n = N.n s
   let n = S N.n
-end
+end;;
+[%%expect{|
+module S : functor (N : N) -> sig type n = N.n s val n : n end
+|}];;
 
 module type ADD = sig
   type a and b and c
   val a : a
   val b : b
   val c : c
-end
+end;;
+[%%expect{|
+module type ADD = sig type a and b and c val a : a val b : b val c : c end
+|}];;
 
-let add {Add : ADD} (a: Add.a) (b : Add.b) : Add.c = Add.c
+let add {Add : ADD} (a: Add.a) (b : Add.b) : Add.c = Add.c;;
+[%%expect{|
+val add : {Add: ADD} -> Add.a -> Add.b -> Add.c = <fun>
+|}];;
 
 implicit module AddZ {B : N} : ADD with type a = z
                                     and type b = B.n
@@ -31,7 +54,12 @@ implicit module AddZ {B : N} : ADD with type a = z
 struct
   type a = z and b = B.n and c = B.n
   let  a = Z and b = B.n and c = B.n
-end
+end;;
+[%%expect{|
+module AddZ :
+  functor (B : N) ->
+    sig type a = z and b = B.n and c = B.n val a : a val b : b val c : c end
+|}];;
 
 implicit module AddS {A: N} {B: N} {Add : ADD with type a = A.n and
 type b = B.n}
@@ -41,10 +69,35 @@ type b = B.n}
 struct
   type a = A.n s and b = B.n and c = Add.c s
   let  a = S A.n and b = B.n and c = S Add.c
-end
-;;
+end;;
+[%%expect{|
+module AddS :
+  functor (A : N) (B : N)
+    (Add : sig
+             type a = A.n
+             and b = B.n
+             and c
+             val a : a
+             val b : b
+             val c : c
+           end)
+    ->
+    sig
+      type a = A.n s
+      and b = B.n
+      and c = Add.c s
+      val a : a
+      val b : b
+      val c : c
+    end
+|}];;
 
 (* Stress Implicitsearch.translpath
    Resolved code: add (implicit AddS(Z)(Z)(AddZ(Z))) (S Z) Z *)
-add (S Z) Z
-;;
+add (S Z) Z;;
+[%%expect{|
+Line 1, characters 0-3:
+1 | add (S Z) Z;;
+    ^^^
+Error: No instance found for implicit Add.
+|}];;
--- testsuite/tests/typing-modular_implicits/implicits.ml
+++ testsuite/tests/typing-modular_implicits/implicits.ml
@@ -1,36 +1,89 @@
+(* TEST
+   * expect
+*)
 
 module type T = sig type t val x : t end;;
+[%%expect{|
+module type T = sig type t val x : t end
+|}];;
 
 (* BAD *)
 let f () =
   let x = ref [] in
   let g {M : T} () = x := [M.x] in
     ();;
+[%%expect{|
+Line 3, characters 27-30:
+3 |   let g {M : T} () = x := [M.x] in
+                               ^^^
+Error: This expression has type M.t but an expression was expected of type 'a
+       The type constructor M.t would escape its scope
+|}];;
 
+(* FIXME! (occur_univar_and_implicit?) *)
 (* BAD *)
 let f (x : 'a) {M : T} =
   (x : M.t);
   ();;
+[%%expect{|
+Line 2, characters 2-11:
+2 |   (x : M.t);
+      ^^^^^^^^^
+Warning 10: this expression should have type unit.
+val f : M/1.t -> {M/2: T} -> unit = <fun>
+|}];;
 
 (* OK *)
 let f {M : T} (x : M.t) y =
   (y : M.t);
   ();;
+[%%expect{|
+Line 2, characters 2-11:
+2 |   (y : M.t);
+      ^^^^^^^^^
+Warning 10: this expression should have type unit.
+val f : {M: T} -> M.t -> M.t -> unit = <fun>
+|}];;
 
 (* OK *)
 let rec f {M : T} (x : M.t) = ();;
+[%%expect{|
+val f : {M: T} -> M.t -> unit = <fun>
+|}];;
 
 (* OK *)
 let rec f {M : T} (x : M.t) y =
   (y : M.t);
   ();;
+[%%expect{|
+Line 2, characters 2-11:
+2 |   (y : M.t);
+      ^^^^^^^^^
+Warning 10: this expression should have type unit.
+val f : {M: T} -> M.t -> M.t -> unit = <fun>
+|}];;
 
+(* FIXME: that error doesn't look right. *)
 (* BAD *)
 let f : {M : T} -> 'a -> unit =
   fun {M : T} (x : M.t) -> ();;
+[%%expect{|
+Line 2, characters 2-29:
+2 |   fun {M : T} (x : M.t) -> ();;
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Error: This expression has type {M/1: T} -> M/1.t -> unit
+       but an expression was expected of type {M/3: T} -> M/2.t -> unit
+       Type M/1.t is not compatible with type M/2.t
+|}];;
 
 (* OK *)
 let f (g : {M : T} -> M.t -> unit) () = ();;
+[%%expect{|
+val f : ({M: T} -> M.t -> unit) -> unit -> unit = <fun>
+|}];;
 
 (* OK *)
 let f {M : T} {N : T} = N.x;;
+[%%expect{|
+val f : {M: T} -> {N: T} -> N.t = <fun>
+|}];;
